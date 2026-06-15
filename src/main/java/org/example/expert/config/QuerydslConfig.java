3c53567ae6da5faf3592f4a0cd5b4e8e7dd7d330
tjdgns0618 <gruddufs@gmail.com>
2026-06-15 19:43:23 +0900

    docs: lv.2-8 QuerydslConfig 주석 추가

--- src/main/java/org/example/expert/config/QuerydslConfig.java
+++ src/main/java/org/example/expert/config/QuerydslConfig.java
@@ -8,6 +8,8 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
+// Querydsl은 기본적으로 DB와 직접적으로 통신하는 기능이 없다
+// 이를 QueryDsl을 만든 곳에서 DB와 통신할 수 있게 하기 위해서 이 설정 코드의 EntityManager를 넘겨받아야만 한다.
 @Configuration
 public class QuerydslConfig {
 
