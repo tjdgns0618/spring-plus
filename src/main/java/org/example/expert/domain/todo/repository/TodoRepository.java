0678fc4bcbb3c2a080a2f500fb674e9c65a556e3
tjdgns0618 <gruddufs@gmail.com>
2026-06-16 14:37:32 +0900

    docs: lv.2-8 사용 안하는 주석 제거

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
