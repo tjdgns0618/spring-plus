86b1da98609352fb1c38f164dd1d9c9931314ade
tjdgns0618 <gruddufs@gmail.com>
2026-06-16 14:37:20 +0900

    docs: lv.2-8 사용 안하는 주석 제거

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -22,9 +22,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRep
         + "ORDER BY t.modifiedAt DESC"
         + "")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable, String weather, LocalDateTime startAt, LocalDateTime endAt);
-
-    // @Query("SELECT t FROM Todo t " +
-    //         "LEFT JOIN t.user " +
-    //         "WHERE t.id = :todoId")
-    // Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
