e79fc86fd13bb89b88001d7efa960cd7d3e9c9c9
tjdgns0618 <gruddufs@gmail.com>
2026-06-19 10:50:54 +0900

    feat: lv.3-13 회원 테이블의 nickname 컬럼에 인덱스를 생성하는 코드 추가

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.user.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.entity.Timestamped;
 import org.example.expert.domain.user.enums.UserRole;
@@ -10,42 +11,44 @@ import org.example.expert.domain.user.enums.UserRole;
 @Getter
 @Entity
 @NoArgsConstructor
-@Table(name = "users")
+@Table(name = "users",
+	indexes = {@Index(name = "idx_user_nickname", columnList = "nickname")})
 public class User extends Timestamped {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    // nickname 추가 설정
-    private String nickname;
-    @Column(unique = true)
-    private String email;
-    private String password;
-    @Enumerated(EnumType.STRING)
-    private UserRole userRole;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	// nickname 추가 설정
+	private String nickname;
+	@Column(unique = true)
+	private String email;
+	private String password;
+	@Enumerated(EnumType.STRING)
+	private UserRole userRole;
 
-    public User(String nickname, String email, String password, UserRole userRole) {
-        this.nickname = nickname;
-        this.email = email;
-        this.password = password;
-        this.userRole = userRole;
-    }
+	public User(String nickname, String email, String password, UserRole userRole) {
+		this.nickname = nickname;
+		this.email = email;
+		this.password = password;
+		this.userRole = userRole;
+	}
 
-    private User(Long id, String nickname, String email, UserRole userRole) {
-        this.id = id;
-        this.nickname = nickname;
-        this.email = email;
-        this.userRole = userRole;
-    }
+	private User(Long id, String nickname, String email, UserRole userRole) {
+		this.id = id;
+		this.nickname = nickname;
+		this.email = email;
+		this.userRole = userRole;
+	}
 
-    public static User fromAuthUser(AuthUser authUser) {
-        return new User(authUser.getId(), authUser.getNickname(), authUser.getEmail(), authUser.getUserRole());
-    }
+	public static User fromAuthUser(AuthUser authUser) {
+		return new User(authUser.getId(), authUser.getNickname(), authUser.getEmail(), authUser.getUserRole());
+	}
 
-    public void changePassword(String password) {
-        this.password = password;
-    }
+	public void changePassword(String password) {
+		this.password = password;
+	}
 
-    public void updateRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
+	public void updateRole(UserRole userRole) {
+		this.userRole = userRole;
+	}
 }
